a62d2bf356981dbb16930c6ce7f1362057d11baf
John <sepulvedaborquez.j@gmail.com>
2025-08-22 01:00:25 -0400

    Actualizar index y app

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+// Función para agregar un amigo
+function agregarAmigo() {
+    let input = document.getElementById("inputName");
+    let nombre = input.value.trim();
+    let resultado = document.getElementById("resultado");
+
+    if (nombre) {
+        amigos.push(nombre);
+        input.value = "";
+        resultado.textContent = `Amigo ${nombre} agregado correctamente.`;
+    } else {
+        resultado.textContent = "Escribe un nombre válido.";
+        return;
+    }
+    actualizarLista();
+}
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // Limpiar la lista actual
+    amigos.forEach((nombre) => {
+        let li = document.createElement("li");
+        li.textContent = nombre;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigosSecreto() {
+    if (amigos.length < 2) {
+        document.getElementById("resultado").textContent = "Necesitas al menos dos amigos para hacer el sorteo.";
+        return;
+    }
+    let indice = Math.floor(Math.random() * amigos.length);
+    let amigoSeleccionado = amigos[indice];
+    document.getElementById("resultado").textContent = `Tu amigo secreto seleccionado es: ${amigoSeleccionado}`;
+}
+
+function mostrarAmigo() {
+    console.log("Lista de amigos:");
+    amigos.forEach((nombre) => {
+        console.log(nombre);
+    });
+}
